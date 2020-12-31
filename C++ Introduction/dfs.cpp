3e92ec81dade6c1b0a3535e1c443726fa2d9513b
freyamurphy <freyamurphy@gmail.com>
2020-12-31 14:32:17 +0000

    Started DFS solution

---
+++ C++ Introduction/dfs.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+
+void solveTest()
+{
+  // n = number of vertices
+  // m = number of edges
+  // v = starting vertex
+  int n, m, v = 0;
+  std::cin >> n >> m >> v;
+  
+  
+  for (int i = 0; i < m; i++)
+  {
+    int a, b = 0;
+    std::cin >> a >> b;
+  }
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+
+    int t = 0; std::cin >> t;   // number of tests
+    
+    for (int i = 0; i < t; i++)
+    {
+      solveTest();
+    }
+}
