37a8bd35b6ea0a46963af69c5b29e7358941c78e
freyamurphy <freyamurphy@gmail.com>
2021-01-19 10:43:40 +0100

    Partial sums solution for even pairs

--- Week1/EvenPairs/evenpairs.cpp
+++ Week1/EvenPairs/evenpairs.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 #include <vector>
 
-/* DEBUG */
-void printVector(const std::vector<int>& v)
-{
-    for (auto it = v.begin(); it != v.end(); it++)
-    {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
-}
-
 void readInput(std::vector<int>& bits)
 {
     int n; std::cin >> n; // number of bits
@@ -22,36 +12,30 @@ void readInput(std::vector<int>& bits)
     }
 }
 
-int bruteForce(const std::vector<int>& bits)
+int partialSums(const std::vector<int>& bits)
 {
     int evenPairs = 0;
 
-    for (int i = 0; i < (int)bits.size(); i++)
+    std::vector<int> partialSums;
+    int sum = 0;
+    for (std::vector<int>::const_iterator it = bits.begin(); it != bits.end(); it++)
     {
-        for (int j = i; j < (int)bits.size(); j++)
-        {
-            // DEBUG
-            //std::cout << "i=" << i << " j=" << j << " "; 
-            int sum = 0;
-            for (int k = i; k <= j; k++)
-            {
-                // DEBUG
-                //std::cout << bits.at(k) << " ";
-                sum += bits.at(k);
-            }
+        sum += *it;
+        partialSums.push_back(sum);
+    }
 
-            if (sum % 2 == 0)
-            {
-                evenPairs++;
-                // DEBUG
-                //std::cout << "EVEN" << std::endl;
-            }
-            // DEBUG
-            //else
-            //{
-                //std::cout << "ODD" << std::endl;
-            //}
-        }
+    for (size_t i = 0; i < bits.size(); i++)
+    {        
+        for (size_t j = i; j < bits.size(); j++)
+        {
+            int sum = 0;
+            if (i == 0) 
+                sum = partialSums.at(j);
+            else
+                sum = partialSums.at(j) - partialSums.at(i-1);
+
+            if (sum % 2 == 0) evenPairs++;
+        }        
     }
 
     return evenPairs;
@@ -61,8 +45,7 @@ void solveTest()
 {
     std::vector<int> bits;
     readInput(bits);
-    //printVector(bits);
-    std::cout << bruteForce(bits) << std::endl;
+    std::cout << partialSums(bits) << std::endl;
 }
 
 int main()
