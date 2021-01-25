9270a447a2031ba3ae886725fbe49cf2ea550f9e
freyamurphy <freyamurphy@gmail.com>
2021-01-25 13:50:10 +0100

    Precomputed-sums solution for Even Matrices

--- Week1/EvenMatrices/evenmatrices.cpp
+++ Week1/EvenMatrices/evenmatrices.cpp
@@ -15,28 +15,48 @@ void readMatrix(std::vector<std::vector<int> >& matrix, int n)
     }
 }
 
+/* Code from solution handout */
+void precomputeSums(std::vector<std::vector<int> >& sums, const std::vector<std::vector<int> >& matrix)
+{
+    // Compute sums[i][j] by using the values sums[i'][j'] for 
+    // i' <= i and j' <= j.
+    for (size_t i = 1; i < sums.size(); ++i)
+    {
+        for (size_t j = 1; j < sums.size(); ++j)
+        {
+            sums.at(i).at(j) = sums.at(i - 1).at(j) + sums.at(i).at(j - 1) - sums.at(i - 1).at(j - 1) + matrix.at(i-1).at(j-1);
+        }
+    }
+}
+
 int countEvenPairs(const std::vector<std::vector<int> >& matrix)
 {
     int evenPairs = 0;
 
-    // Go through all possible quadruples
-    for (size_t i1 = 0; i1 < matrix.size(); i1++)
+    // Precompute sums
+    std::vector<std::vector<int> > sums;
+    for (size_t i = 0; i < matrix.size()+1; i++)
     {
-        for (size_t i2 = i1; i2 < matrix.size(); i2++)
+        std::vector<int> row;
+        for (size_t j = 0; j < matrix.size()+1; j++)
         {
-            for (size_t j1 = 0; j1 < matrix.size(); j1++)
+            row.push_back(0);
+        }
+        sums.push_back(row);
+    }
+    precomputeSums(sums, matrix);
+
+    // Go through all possible quadruples
+    for (size_t i1 = 1; i1 < sums.size(); i1++)
+    {
+        for (size_t i2 = i1; i2 < sums.size(); i2++)
+        {
+            for (size_t j1 = 1; j1 < sums.size(); j1++)
             {
-                for (size_t j2 = j1; j2 < matrix.size(); j2++)
+                for (size_t j2 = j1; j2 < sums.size(); j2++)
                 {
-                    // Add up everything in this range
-                    int sum = 0;
-                    for (size_t k = i1; k <= i2; k++)
-                    {
-                        for (size_t l = j1; l <= j2; l++)
-                        {
-                            sum += matrix.at(k).at(l);
-                        }
-                    }
+                    // Formula from solution handout
+                    int sum = sums.at(i2).at(j2) - sums.at(i2).at(j1 - 1) - sums.at(i1 - 1).at(j2) + sums.at(i1 - 1).at(j1 - 1);
 
                     if (sum % 2 == 0)
                     {
