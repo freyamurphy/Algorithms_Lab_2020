bd23dc7dd61b9e13a4d3f6235ec14a1b9a2e8b04
freyamurphy <freyamurphy@gmail.com>
2021-01-27 11:46:06 +0100

    More efficient solution to Burning Coins

--- Week2/BurningCoins/burningcoins.cpp
+++ Week2/BurningCoins/burningcoins.cpp
@@ -1,100 +1,56 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <map>
 
-void printVector(std::vector<int>* vector)
+void readCoins(std::vector<int>& coins, int n)
 {
-  for (std::vector<int>::iterator it = vector->begin(); it != vector->end();
-       it++)
+  for (int i = 0; i < n; i++)
   {
-    std::cout << *it << " ";
-  }
-  std::cout << "\n";
-}
-
-void readCoins(std::vector<int>* coins)
-{
-  int value = 0;
-  for (std::vector<int>::iterator it = coins->begin(); it != coins->end(); it++)
-  {
-    std::cin >> value;
-    *it = value;
+    int v; std::cin >> v; // value of coin
+    coins.push_back(v);
   }
 }
 
-int guaranteedWinnings(std::vector<int>* coins, bool myTurn, 
-                       std::map<std::vector<int>, int>* memoMyTurn, 
-                       std::map<std::vector<int>, int>* memoTheirTurn)
+int guaranteedWinnings(const std::vector<int>& coins, std::vector<std::vector<int> >& memo, int left, 
+  int right, bool myTurn)
 {
-  if (myTurn)
+  if (left == right)
   {
-    if (coins->size() == 1)
-    {
-      return coins->at(0);
-    }
-
-    if (memoMyTurn->find(*coins) == memoMyTurn->end())
-    {
-      std::vector<int> splitAtStart(coins->begin() + 1, coins->end());
-      std::vector<int> splitAtEnd(coins->begin(), coins->end() - 1);
-      int choice1 = coins->at(0) + guaranteedWinnings(&splitAtStart, !myTurn,
-        memoMyTurn, memoTheirTurn);
-      int choice2 = guaranteedWinnings(&splitAtEnd, !myTurn, memoMyTurn,
-        memoTheirTurn) + coins->back();
-      int winnings = std::max(choice1, choice2);
-      memoMyTurn->insert(make_pair(*coins, winnings));
-    }
-    
-    return memoMyTurn->at(*coins);
-  }
-  else
-  {
-    if (coins->size() == 1)
-    {
+    if (myTurn)
+      return coins.at(left);
+    else
       return 0;
-    }
-    
-    if (memoTheirTurn->find(*coins) == memoTheirTurn->end())
-    {
-      std::vector<int> splitAtStart(coins->begin() + 1, coins->end());
-      std::vector<int> splitAtEnd(coins->begin(), coins->end() - 1);
-      int choice1 = guaranteedWinnings(&splitAtStart, !myTurn, memoMyTurn, 
-        memoTheirTurn);
-      int choice2 = guaranteedWinnings(&splitAtEnd, !myTurn, memoMyTurn, 
-        memoTheirTurn);
-      int winnings = std::min(choice1, choice2); 
-      // Opponent with choose option that minimises my winnings
-      memoTheirTurn->insert(make_pair(*coins, winnings));
-    }
-    return memoTheirTurn->at(*coins);
   }
+
+  int chooseLeft = coins.at(left) + guaranteedWinnings(coins, memo, left + 1, right, !myTurn);
+  int chooseRight = coins.at(right) + guaranteedWinnings(coins, memo, left, right - 1, !myTurn);
+
+  int winnings = 0;
+  if (myTurn)
+    winnings = std::max(chooseLeft, chooseRight);
+  else
+    winnings = std::min(chooseLeft - coins.at(left), chooseRight - coins.at(right));
+
+  return winnings;
 }
 
-void solveTest(std::map<std::vector<int>, int>* memoMyTurn,
-               std::map<std::vector<int>, int>* memoTheirTurn)
+void solveTest()
 {
-  int n = 0;
-  std::cin >> n;
-  
-  std::vector<int> coins(n);
-  readCoins(&coins);
-  
-  std::cout << guaranteedWinnings(&coins, true, memoMyTurn, memoTheirTurn);
-  std::cout << std::endl;
+  int n; std::cin >> n; // number of coins
+  std::vector<int> coins;
+  readCoins(coins, n);
+  std::vector<std::vector<int> > memo;
+  std::cout << guaranteedWinnings(coins, memo, 0, coins.size()-1, true) << std::endl;
 }
 
 int main()
 {
   std::ios_base::sync_with_stdio(false);
-  
-  std::map<std::vector<int>, int> memoMyTurn;
-  std::map<std::vector<int>, int> memoTheirTurn;
-  
-  int t = 0;
-  std::cin >> t;
+
+  int t; std::cin >> t; // number of test cases
   for (int i = 0; i < t; i++)
   {
-    solveTest(&memoMyTurn, &memoTheirTurn);
+    solveTest();
   }
+
+  return 0;
 }
